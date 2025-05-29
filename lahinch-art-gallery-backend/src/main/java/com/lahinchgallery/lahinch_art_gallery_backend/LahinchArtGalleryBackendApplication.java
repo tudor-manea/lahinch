6ee08eaf7e0f7f51fb6e629c81d980cfe818afc5
tudor-manea <tudor.manea03@gmail.com>
2025-05-29 13:46:45 +0100

    backend initial setup

---
+++ lahinch-art-gallery-backend/src/main/java/com/lahinchgallery/lahinch_art_gallery_backend/LahinchArtGalleryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lahinchgallery.lahinch_art_gallery_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LahinchArtGalleryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LahinchArtGalleryBackendApplication.class, args);
+	}
+
+}
